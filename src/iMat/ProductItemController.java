f9e32ca8fbd07416eca7bfa22429a8943329fe70
Falckishen <samuelfa@student.chalmers.se>
2022-05-09 19:30:15 +0200

    La till en konstruktor i ProductItemController

--- src/iMat/ProductItemController.java
+++ src/iMat/ProductItemController.java
@@ -1,9 +1,28 @@
+/* Hör ihop med ProductItem.fxml */
 package iMat;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
+import java.io.IOException;
 
-public class ProductItemController {
+public class ProductItemController extends AnchorPane {
 
     private final IMatDataHandler dataHandler = IMat.getIMatDataHandler();
+    private final Product product;
 
+    public ProductItemController(Product product) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ProductItem.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+
+        this.product = product;
+    }
 }
