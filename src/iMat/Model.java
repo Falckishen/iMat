1d3910e694d23520976e7a99b184abf37b3de002
Falckishen <samuelfa@student.chalmers.se>
2022-05-09 12:59:27 +0200

    Fixade lite kod

--- src/iMat/Model.java
+++ src/iMat/Model.java
@@ -1,28 +1,11 @@
 package iMat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import javafx.scene.image.Image;
-import se.chalmers.cse.dat216.project.CreditCard;
-import se.chalmers.cse.dat216.project.Customer;
 import se.chalmers.cse.dat216.project.IMatDataHandler;
-import se.chalmers.cse.dat216.project.Product;
-import se.chalmers.cse.dat216.project.ShoppingCart;
-import se.chalmers.cse.dat216.project.ShoppingItem;
 
 public class Model {
 
     private static Model instance = null;
     private IMatDataHandler iMatDataHandler;
-
-     private final ArrayList<String> availableCardTypes = new ArrayList<String>(Arrays.asList("MasterCard", "Visa"));
-     private final ArrayList<String> months = new ArrayList<String>(Arrays.asList("1", "2","3", "4", "5", "6"));
-     private final ArrayList<String> years = new ArrayList<String>(Arrays.asList("19", "20", "21", "22", "23", "24", "25"));
-
-    protected Model() {
-        // Exists only to defeat instantiation.
-    }
 
     public static Model getInstance() {
         if (instance == null) {
@@ -34,70 +17,6 @@ public class Model {
 
     private void init() {
         iMatDataHandler = IMatDataHandler.getInstance();
-    }
-
-    public List<Product> getProducts() {
-        return iMatDataHandler.getProducts();
-    }
-
-    public Product getProduct(int idNbr) {
-        return iMatDataHandler.getProduct(idNbr);
-    }
-    
-    public List<Product> findProducts(java.lang.String s) {
-        return iMatDataHandler.findProducts(s);
-    }
-
-    public Image getImage(Product p) {
-        return iMatDataHandler.getFXImage(p);
-    }
-
-    public Image getImage(Product p, double width, double height) {
-        return iMatDataHandler.getFXImage(p, width, height);
-    }
-
-    public void addToShoppingCart(Product p) {
-        ShoppingCart shoppingCart = iMatDataHandler.getShoppingCart();
-        ShoppingItem item = new ShoppingItem(p);
-        Model.getInstance().getShoppingCart().addItem(item);
-        
-        //shoppingCart.addProduct(p);
-    }
-
-    public List<String> getCardTypes() {
-        return availableCardTypes;
-    }
-    
-    public List<String> getMonths() {
-        return months;
-    }
-    
-    public List<String> getYears() {
-        return years;
-    }
-    
-    public CreditCard getCreditCard() {
-        return iMatDataHandler.getCreditCard();
-    }
-    
-    public Customer getCustomer() {
-        return iMatDataHandler.getCustomer();
-    }
-
-    public ShoppingCart getShoppingCart() {
-        return iMatDataHandler.getShoppingCart();
-    }
-
-    public void clearShoppingCart() {
-        iMatDataHandler.getShoppingCart().clear();
-    }
-
-    public void placeOrder() {
-        iMatDataHandler.placeOrder();
-    }
-    
-    public int getNumberOfOrders() {
-        return iMatDataHandler.getOrders().size();
     }
 
     public void shutDown() {
