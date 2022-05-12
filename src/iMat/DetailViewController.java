674549aac7456b19240f35b71c2a51e25f782daa
Falckishen <samuelfa@student.chalmers.se>
2022-05-12 17:45:21 +0200

    Ändrade sättet att ladda DetailView.fxml

---
+++ src/iMat/DetailViewController.java
@@ -0,0 +1,97 @@
+// Hör ihop med DetailView.fxml
+package iMat;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
+import java.util.Objects;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.image.*;
+import se.chalmers.cse.dat216.project.*;
+
+public class DetailViewController extends AnchorPane {
+
+    private final IMatDataHandler dataHandler = IMat.getIMatDataHandler();
+    private final ClassLoader classLoader = getClass().getClassLoader();
+
+    private Product product;
+
+    @FXML private ImageView image;
+    @FXML private Label productNameLabel;
+    @FXML private Label priceLabel;
+    @FXML private TextField numOfItems;
+    @FXML private Label isEcoLabel;
+    @FXML private ImageView closeImageView;
+
+    public DetailViewController(Product product) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("DetailView.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        this.product = product;
+        populateProductDetailView();
+    }
+
+    @FXML
+    public void closeProductDetailView() {
+        this.toBack();
+    }
+
+    @FXML
+    public void closeButtonMouseEntered() {
+        closeImageView.setImage(new Image(Objects.requireNonNull(classLoader.getResourceAsStream("iMat/images/icon_close_hover.png"))));
+    }
+
+    @FXML
+    public void closeButtonMousePressed() {
+        closeImageView.setImage(new Image(Objects.requireNonNull(classLoader.getResourceAsStream("iMat/images/icon_close_pressed.png"))));
+    }
+
+    @FXML
+    public void closeButtonMouseExited(){
+        closeImageView.setImage(new Image(Objects.requireNonNull(classLoader.getResourceAsStream("iMat/images/icon_close.png"))));
+    }
+
+    @FXML
+    public void mouseTrap(Event event) {
+        event.consume();
+    }
+
+    // Fyller upp detalj vyn med rätt produkt
+    private void populateProductDetailView() {
+        image.setImage(dataHandler.getFXImage(this.product));
+        productNameLabel.setText(this.product.getName());
+        priceLabel.setText(String.format("Pris: %.2f %s", this.product.getPrice(), this.product.getUnit()));
+        if (this.product.isEcological()) {
+            isEcoLabel.setText("Produkten är ekologisk");
+        }
+        else {
+            isEcoLabel.setText("Produkten är ej ekologisk");
+        }
+        numOfItems.setText(String.format("%.1f", IMat.getNumberOfAProductInCart(this.product)));
+    }
+}
--- src/iMat/MainPageController.java
+++ src/iMat/MainPageController.java
@@ -1,7 +1,6 @@
-// Hör ihop med IMatMainPage.fxml
+// Hör ihop med MainPage.fxml
 package iMat;
 
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -11,14 +10,10 @@ import java.util.ResourceBundle;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.*;
 import javafx.scene.Node;
-import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.image.*;
 import se.chalmers.cse.dat216.project.*;
@@ -30,17 +25,8 @@ public class MainPageController implements Initializable {
 
     private final AccountWindowController accountWindowController = new AccountWindowController();
 
-    private final ClassLoader classLoader = getClass().getClassLoader();
-
     @FXML private AnchorPane mainPageRootAnchorPane;
-    @FXML private AnchorPane detailViewAnchorPane;
-    @FXML private ImageView detailViewImage;
-    @FXML private Label detailViewProductNameLabel;
-    @FXML private Label detailViewPriceLabel;
-    @FXML private TextField detailViewNumOfItems;
-    @FXML private Label detailViewIsEcoLabel;
     @FXML private FlowPane productItemsFlowpane;
-    @FXML private ImageView closeImageView;
     @FXML private Button brodKnapp;
     @FXML private TextField searchBar;
     @FXML private Button btnTestKonto;
@@ -48,20 +34,15 @@ public class MainPageController implements Initializable {
     // Körs när MainPage.fxml läses in
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        populateProductDetailView(dataHandler.getProduct(75)); // Temporär, används för test
+        // populateProductDetailView(dataHandler.getProduct(75)); // Temporär, används för test
         fillFood();
     }
 
     @FXML
-    public void openProductDetailView(Product product) {
-        populateProductDetailView(product);
+    public void openProductDetailView(Product product) throws IOException {
+        AnchorPane detailViewAnchorPane = new DetailViewController(product);
+        mainPageRootAnchorPane.getChildren().add(detailViewAnchorPane);
         detailViewAnchorPane.toFront();
-
-    }
-
-    @FXML
-    public void closeProductDetailView() {
-        detailViewAnchorPane.toBack();
     }
 
     @FXML
@@ -69,26 +50,6 @@ public class MainPageController implements Initializable {
         AnchorPane accountWindowPane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AccountWindow.fxml")));
         mainPageRootAnchorPane.getChildren().add(accountWindowPane);
         accountWindowPane.toFront();
-    }
-
-    @FXML
-    public void closeButtonMouseEntered() {
-        closeImageView.setImage(new Image(Objects.requireNonNull(classLoader.getResourceAsStream("iMat/images/icon_close_hover.png"))));
-    }
-
-    @FXML
-    public void closeButtonMousePressed() {
-        closeImageView.setImage(new Image(Objects.requireNonNull(classLoader.getResourceAsStream("iMat/images/icon_close_pressed.png"))));
-    }
-
-    @FXML
-    public void closeButtonMouseExited(){
-        closeImageView.setImage(new Image(Objects.requireNonNull(classLoader.getResourceAsStream("iMat/images/icon_close.png"))));
-    }
-
-    @FXML
-    public void mouseTrap(Event event) {
-        event.consume();
     }
 
     /* Knappar below tänker att den fungerar som den ska, fortsätter på allt annat och återvänder till det här när mera
@@ -110,20 +71,6 @@ public class MainPageController implements Initializable {
         for(Product product : productList){
             productItemsList.add(new ProductItemController(product));
         }
-    }
-
-    // Fyller upp detalj vyn med rätt produkt
-    private void populateProductDetailView(Product product) {
-        detailViewImage.setImage(dataHandler.getFXImage(product));
-        detailViewProductNameLabel.setText(product.getName());
-        detailViewPriceLabel.setText(String.format("Pris: %.2f %s", product.getPrice(), product.getUnit()));
-        if (product.isEcological()) {
-            detailViewIsEcoLabel.setText("Produkten är ekologisk");
-        }
-        else {
-            detailViewIsEcoLabel.setText("Produkten är ej ekologisk");
-        }
-        detailViewNumOfItems.setText(String.format("%.1f", IMat.getNumberOfAProductInCart(product)));
     }
 
     //TODO: Populera med rätt grejor tex. historiken.
