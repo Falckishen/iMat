37544cd79ca6e447cc009ec378a8a6e75e1beab7
Clapped99 <andres.arriagada@hotmail.com>
2022-05-12 18:15:58 +0200

    Flowpane changed to Grid, minor quirks though...

--- src/iMat/MainPageController.java
+++ src/iMat/MainPageController.java
@@ -30,6 +30,9 @@ public class MainPageController implements Initializable {
     @FXML private Button brodKnapp;
     @FXML private TextField searchBar;
     @FXML private Button btnTestKonto;
+    @FXML private GridPane gridPane;
+    private int rowx = 0;
+    private int coly = 0;
 
     // Körs när MainPage.fxml läses in
     @Override
@@ -55,22 +58,51 @@ public class MainPageController implements Initializable {
     /* Knappar below tänker att den fungerar som den ska, fortsätter på allt annat och återvänder till det här när mera
     är färdigt, hoppas det är okej.*/
     @FXML protected void searchForBread (ActionEvent event) {
-        ObservableList<Node> productItemsList = productItemsFlowpane.getChildren();
+        coly = 0;
+        rowx = 0;
+        gridPane.getChildren().clear();
+        ArrayList<Product> productlist = (ArrayList<Product>) dataHandler.getProducts(ProductCategory.BREAD);
+        for(Product product: productlist){
+            ProductItemController productt = new ProductItemController(product);
+            gridPane.add(productt,coly,rowx);
+            coly++;
+            if(coly == 2 ){
+                coly = 0;
+                rowx++;
+            }}
+
+        /*ObservableList<Node> productItemsList = productItemsFlowpane.getChildren();
         productItemsList.clear();
         ArrayList<Product> productList = (ArrayList<Product>) dataHandler.getProducts(ProductCategory.valueOf("BREAD"));
         for(Product product : productList){
             productItemsList.add(new ProductItemController(product));
-        }
+        }*/
     }
 
     @FXML protected void searchBar (ActionEvent event) {
-        ObservableList<Node> productItemsList = productItemsFlowpane.getChildren();
+        coly = 0;
+        rowx = 0;
+        gridPane.getChildren().clear();
+        String text = searchBar.getText();
+        ArrayList<Product> productlist = (ArrayList<Product>) dataHandler.findProducts(text);
+        for(Product product: productlist){
+            ProductItemController productt = new ProductItemController(product);
+            gridPane.add(productt,coly,rowx);
+            coly++;
+            if(coly == 2 ){
+                coly = 0;
+                rowx++;
+            }}
+
+
+
+        /*ObservableList<Node> productItemsList = productItemsFlowpane.getChildren();
         productItemsList.clear();
         String text = searchBar.getText();
         ArrayList<Product> productList = (ArrayList<Product>) dataHandler.findProducts(text);
         for(Product product : productList){
             productItemsList.add(new ProductItemController(product));
-        }
+        }*/
     }
 
     //TODO: Populera med rätt grejor tex. historiken.
@@ -93,11 +125,23 @@ public class MainPageController implements Initializable {
 
     //Fyller på flowpanen för att bygga all funktionalitet runt
     private void fillFood() {
-        ObservableList<Node> productItemsList = productItemsFlowpane.getChildren();
+        coly = 0;
+        rowx = 0;
+        gridPane.getChildren().clear();
+        ArrayList<Product> productlist = (ArrayList<Product>) dataHandler.getProducts();
+        for(Product product: productlist){
+            ProductItemController productt = new ProductItemController(product);
+            gridPane.add(productt,coly,rowx);
+            coly++;
+            if(coly == 2 ){
+                coly = 0;
+                rowx++;
+            }
+
+        /*ObservableList<Node> productItemsList = productItemsFlowpane.getChildren();
         productItemsList.clear();
         ArrayList<Product> productList = (ArrayList<Product>) dataHandler.getProducts();
         for(Product product: productList){
             productItemsList.add(new ProductItemController(product));
-        }
-    }
-}
+        }*/
+    }}}
