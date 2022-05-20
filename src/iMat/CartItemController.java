57d2fabeed471441369c703373a5e4be87b78223
Julia <juliakaufmann@live.se>
2022-05-20 22:09:35 +0200

    Small  uppdates

--- src/iMat/CartItemController.java
+++ src/iMat/CartItemController.java
@@ -2,11 +2,13 @@
 package iMat;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
@@ -19,12 +21,15 @@ public class CartItemController extends AnchorPane {
 
     private final IMatDataHandler dataHandler = IMat.getIMatDataHandler();
     private final MainPageController mainPageController;
+    private final ClassLoader classLoader = getClass().getClassLoader();
+
     private final Product product;
 
     @FXML private ImageView image;
     @FXML private Label nameText;
     @FXML private Label priceText;
     @FXML private Label numberOfProducts;
+    @FXML private ImageView closeImageView;
 
     public CartItemController(ShoppingItem item, MainPageController mainPageController) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/CartItem.fxml"));
@@ -60,6 +65,16 @@ public class CartItemController extends AnchorPane {
     private void minusButtonClicked(Event event) {
         event.consume();
         IMat.removeOneFromCart(this.product);
+    }
+
+    @FXML
+    private void closeButtonMouseEnter() {
+        this.closeImageView.setImage(new Image(Objects.requireNonNull(classLoader.getResourceAsStream("iMat/images/cross_close_white.png"))));
+    }
+
+    @FXML
+    private void closeButtonMouseExited() {
+        this.closeImageView.setImage(new Image(Objects.requireNonNull(classLoader.getResourceAsStream("iMat/images/cross_close_hover.png"))));
     }
 
     @FXML
