c3f09472b0ae2c0cad8e2dd8e3fc9343e3660549
Clapped99 <andres.arriagada@hotmail.com>
2022-05-26 20:21:30 +0200

    small changes made to cart

--- src/iMat/CartItemController.java
+++ src/iMat/CartItemController.java
@@ -50,6 +50,8 @@ public class CartItemController extends AnchorPane {
 
 /*-------------------------------------------------------------------------------------------------------------------*/
 
+    /**/
+
     @FXML
     private void openDetailView() {
         mainPageController.openProductDetailView(product);
